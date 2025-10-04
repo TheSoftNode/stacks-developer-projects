765797843bc6c694ef020479d66e39d1d1595d98
TheSoftNode <thesoftnode@gmail.com>
2025-10-04 08:57:40 +0100

    Refactor test setup and imports in amm.test.ts
    Reorganized test account variables and updated imports to include Cl from @stacks/transactions. Removed example tests and added contract principal initializations for mock tokens to improve test clarity and maintainability.

--- stacks-amm/contract/tests/amm.test.ts
+++ stacks-amm/contract/tests/amm.test.ts
@@ -1,21 +1,11 @@
-
-import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { beforeEach, describe, expect, it } from "vitest";
 
 const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+const deployer = accounts.get("deployer")!;
+const alice = accounts.get("wallet_1")!;
+const bob = accounts.get("wallet_2")!;
+const charlie = accounts.get("wallet_3")!;
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
-
-describe("example tests", () => {
-  it("ensures simnet is well initialised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
-
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
-});
+const mockTokenOne = Cl.contractPrincipal(deployer, "mock-token");
+const mockTokenTwo = Cl.contractPrincipal(deployer, "mock-token-2");

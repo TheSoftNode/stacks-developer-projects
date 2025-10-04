f30bfe740ffcabcd758d542fe42e2fd108cf0880
TheSoftNode <thesoftnode@gmail.com>
2025-10-04 08:58:32 +0100

    Add helper functions for AMM contract tests
    Introduces utility functions for pool creation, liquidity management, token swaps, and pool ID retrieval to simplify and organize AMM contract test cases.

--- stacks-amm/contract/tests/amm.test.ts
+++ stacks-amm/contract/tests/amm.test.ts
@@ -9,3 +9,69 @@ const charlie = accounts.get("wallet_3")!;
 
 const mockTokenOne = Cl.contractPrincipal(deployer, "mock-token");
 const mockTokenTwo = Cl.contractPrincipal(deployer, "mock-token-2");
+
+
+function createPool() {
+  return simnet.callPublicFn(
+    "amm",
+    "create-pool",
+    [mockTokenOne, mockTokenTwo, Cl.uint(500)],
+    alice
+  );
+}
+
+function addLiquidity(account: string, amount0: number, amount1: number) {
+  return simnet.callPublicFn(
+    "amm",
+    "add-liquidity",
+    [
+      mockTokenOne,
+      mockTokenTwo,
+      Cl.uint(500),
+      Cl.uint(amount0),
+      Cl.uint(amount1),
+      Cl.uint(0),
+      Cl.uint(0),
+    ],
+    account
+  );
+}
+
+function removeLiquidity(account: string, liquidity: number) {
+  return simnet.callPublicFn(
+    "amm",
+    "remove-liquidity",
+    [mockTokenOne, mockTokenTwo, Cl.uint(500), Cl.uint(liquidity)],
+    account
+  );
+}
+
+function swap(account: string, inputAmount: number, zeroForOne: boolean) {
+  return simnet.callPublicFn(
+    "amm",
+    "swap",
+    [
+      mockTokenOne,
+      mockTokenTwo,
+      Cl.uint(500),
+      Cl.uint(inputAmount),
+      Cl.bool(zeroForOne),
+    ],
+    account
+  );
+}
+
+function getPoolId() {
+  return simnet.callReadOnlyFn(
+    "amm",
+    "get-pool-id",
+    [
+      Cl.tuple({
+        "token-0": mockTokenOne,
+        "token-1": mockTokenTwo,
+        fee: Cl.uint(500),
+      }),
+    ],
+    alice // this is a read-only function so user address doesn't matter
+  );
+}
